be615b3719cb5ad2235f13fab8f10dfc4c4f736a
Francesca Giordano <francesca020406@hotmail.com>
2017-11-09 23:32:41 -0500

    yay parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { FormsModule }   from '@angular/forms';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: ContactListComponent },
+  // { path: 'contact/:id', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  FormsModule,
+  RouterModule.forRoot(routes)  //  <!-- "routes" is the array defined above
   ],
   providers: [],
   bootstrap: [AppComponent]
